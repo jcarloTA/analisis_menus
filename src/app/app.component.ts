36456680dcb909d3c32d9a4447593943690453c7
Jencarlo Ticun <jencarlo.ticun@3pillar.corp>
2021-08-28 18:56:36 -0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menus-analisis';
+  
+  @ViewChild('americamenu') americamenu: MatMenuTrigger;
+  @ViewChild('europamenu') europamenu: MatMenuTrigger;
+
+  constructor() {
+    this.initKeyboardEvents();
+  }
+
+  initKeyboardEvents() {
+    window.addEventListener('keydown', (event: KeyboardEvent) => {
+      // Fall back to event.which if event.keyCode is null
+      console.log('event key', event.keyCode)
+      this.openMenu(event.keyCode);
+     });
+  }
+
+
+  openMenu(key: number) {
+    switch(key){
+      case 49: {
+        if(this.americamenu.menuOpen) {
+          this.americamenu.closeMenu();
+        } else {
+          this.americamenu.openMenu();
+        }
+        break;
+      }
+      case 50: {
+        if(this.europamenu.menuOpen) {
+          this.europamenu.closeMenu();
+        } else {
+          this.europamenu.openMenu();
+        }
+        break;
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
